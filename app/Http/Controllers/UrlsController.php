e5596aabd3cb1147137bac42f03a990441efeb13
LancemDev <munyaolance1@gmail.com>
2024-01-10 09:57:26 +0300

    :)

--- app/Http/Controllers/UrlsController.php
+++ app/Http/Controllers/UrlsController.php
@@ -49,8 +49,6 @@ class UrlsController extends Controller
             'description' => $request->description,
         ]);
         return redirect()->route('urls.index');
-
-        
     }
 
     /**
